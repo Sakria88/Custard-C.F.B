d2024d33970bacf71c795854ee09e886fdb0e38c
ks38449 <ksschool64@gmail.com>
2024-11-12 18:35:29 +0000

    Platforms

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private bool collapsable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collapsable = true)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                animator.SetBool("step time", true);
+
+            }
+        }
+        
+
+    }
+}
